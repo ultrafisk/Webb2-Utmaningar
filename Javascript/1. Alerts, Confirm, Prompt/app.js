79e4bffe39b25c4f5a9f9e9f6744838225e737ff
Erik Hacker <ultrafisk@users.noreply.github.com>
2018-03-19 10:56:30 +0100

    comments

--- Javascript/1. Alerts, Confirm, Prompt/app.js
+++ Javascript/1. Alerts, Confirm, Prompt/app.js
@@ -6,7 +6,7 @@ const promptBut = document.querySelector('#prompt');
 const result = document.getElementById('result');
 
 // Lägg till click-event listeners för knapparna som anropar varsin funktion/metod
-
+// kollar om man clickar på knappen
 alertBut.addEventListener('click', doAlert)
 
 confirmBut.addEventListener('click', doConfirm)
@@ -16,21 +16,24 @@ promptBut.addEventListener('click', doPrompt)
 // Deklarera funktionerna/metoderna här
 
 function doAlert() {
+    //gör alert, skriver text i h2
     alert('Hello!');
     result.textContent = 'Alert Used!';
 }
 
 function doConfirm() {
+    //true/false-variabel, true om man clickar OK. skriver ut olika saker beroende på om man clickade ok eller cancel
     var ok = confirm('Are you sure?');
     if (ok == true) {
-        result.textContent = 'Great!'
+        result.textContent = 'Great!';
     }
     else {
-        result.textContent = 'Too bad!'
+        result.textContent = 'Too bad!';
     }
 }
 
 function doPrompt() {
+    //textvariabel som = input i prompten, skriver sedan ut variabeln i result
     var userName = prompt('Name?');
     result.textContent = userName;
 }
